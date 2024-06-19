9998ee391b99444c6a905ffb70322b55f647241d
tokarski-to <tomasz.tokarski93@gmail.com>
2024-06-19 10:30:49 +0200

    Small structural changes in folder structure

--- Home.py
+++ Home.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-folder = 'The-Best-Company-Website'
 
-st.set_page_config(layout='wide')
+st.set_page_config(layout='wide', page_title='The Best Company')
 st.header('The Best Company')
 description = """Nvidia Corporation[a][b] (/ɛnˈvɪdiə/, en-VID-ee-ə) is an American multinational corporation and technology company headquartered
  in Santa Clara, California, and incorporated in Delaware. It is a software and fabless company which designs and supplies graphics processing 
@@ -14,7 +13,7 @@ st.write(description)
 
 st.subheader('Our Team')
 
-df = pd.read_csv(f'{folder}/data.csv')
+df = pd.read_csv(f'data.csv')
 
 col1, empty1, col2, empty2, col3, empty3 = st.columns([1.5, 0.5, 1.5, 0.5, 1.5, 1.5])
 
@@ -23,20 +22,20 @@ with col1:
         full_name = f'{row['first name'].title()} {row['last name'].title()}'
         st.subheader(full_name)
         st.write(row['role'])
-        st.image(f"{folder}/images/{row['image']}")
+        st.image(f"images/{row['image']}")
 
 with col2:
     for index, row in df[4:8].iterrows():
         full_name = f'{row['first name'].title()} {row['last name'].title()}'
         st.subheader(full_name)
         st.write(row['role'])
-        st.image(f"{folder}/images/{row['image']}")
+        st.image(f"images/{row['image']}")
 
 with col3:
     for index, row in df[8:].iterrows():
         full_name = f'{row['first name'].title()} {row['last name'].title()}'
         st.subheader(full_name)
         st.write(row['role'])
-        st.image(f"{folder}/images/{row['image']}")
+        st.image(f"images/{row['image']}")
 
 
